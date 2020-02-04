9f25947d3ad3a155826cd038d4e2d464a01412a9
Chen Yu Pao <windperson@gmail.com>
2020-02-04 19:54:52 +0800

    Add project files.

---
+++ DemoCallExtDynamic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ExtensionLib;
+
+namespace DemoCallExtDynamic
+{
+    using System.Linq;
+    using Util;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Call Extension method normally ===");
+
+            var utcNow = DateTime.UtcNow;
+
+            var iso8601str = utcNow.ToIso8601String(true);
+
+            Console.WriteLine($"UTC Now = {{{utcNow}}}\r\nISO8601 : {{{iso8601str}}}");
+
+            Console.WriteLine("\r\n=== Call Extension method using reflection ===");
+
+            var extensionLibAssembly = AssemblyUtil.TryLoadAssembly("ExtensionLib");
+            if (extensionLibAssembly == null)
+            {
+                throw new Exception("Can not locate target Assembly!");
+            }
+            var targetExtMethod = extensionLibAssembly.GetExtensionMethods(typeof(string), "FromIso8601String").FirstOrDefault();
+
+            var invokeResult = targetExtMethod.Invoke(null, new[] { iso8601str });
+
+            if (invokeResult is DateTime convertBackDatetime)
+            {
+                Console.WriteLine($"Result is {{{convertBackDatetime}}}");
+            }
+        }
+    }
+}
---
+++ DemoCallExtDynamic/Util/AssemblyUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace DemoCallExtDynamic.Util
+{
+    static class AssemblyUtil
+    {
+        public static Assembly TryLoadAssembly(string name)
+        {
+            var loadedAssembly =
+                AppDomain.CurrentDomain.GetAssemblies()
+                .FirstOrDefault(c => c.FullName.Contains(name));
+
+            if (loadedAssembly != null) { return loadedAssembly; }
+
+            var assemblyName = Assembly.GetExecutingAssembly().GetReferencedAssemblies()
+                .FirstOrDefault(c => c.FullName.Contains(name));
+
+            return assemblyName == null ? null : Assembly.Load(assemblyName);
+        }
+
+        public static IEnumerable<MethodInfo> GetExtensionMethods(this Assembly assembly, Type extendedType, string extensionMethodName)
+        {
+            var extMethods =
+                from type in assembly.GetTypes()
+                where type.IsSealed && !type.IsGenericType && !type.IsNested
+                from method in type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+                where method.IsDefined(typeof(ExtensionAttribute), false)
+                where method.Name == extensionMethodName
+                where method.GetParameters()[0].ParameterType == extendedType
+                select method;
+
+            return extMethods;
+        }
+    }
+}
